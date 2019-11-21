cb027e6a760d1b21bad506b69eecc04be3f5642d
歐東 <43561001+jerry960331@users.noreply.github.com>
2019-11-21 17:49:12 +0800

    191120 course

---
+++ src/JavaClass/Ex191120ClassConception2/Ex2CRectangle.java
@@ -0,0 +1,25 @@
+package JavaClass.Ex191120ClassConception2;
+
+public class Ex2CRectangle {
+    public static void main(String[] args) {
+        CRectangle rectangle = new CRectangle();
+        rectangle.showParameter();
+    }
+}
+class CRectangle{
+    int width;
+    int height;
+
+    public CRectangle(int w, int h) {
+        width = w;
+        height = h;
+    }
+    CRectangle(){
+        //width = 10;
+        //height = 8;
+        this(10,8);
+    }
+    void showParameter(){
+        System.out.printf("Width: %d Height: %d", width, height);
+    }
+}
---
+++ src/JavaClass/Ex191120ClassConception2/Ex3CCount.java
@@ -0,0 +1,27 @@
+package JavaClass.Ex191120ClassConception2;
+
+public class Ex3CCount {
+    public static void main(String[] args) {
+        System.out.println(CCount.count());
+        CCount count1 = new CCount();
+        System.out.println(CCount.count());
+        CCount count2 = new CCount();
+        CCount count3 = new CCount();
+        System.out.println(count3.count());
+    }
+}
+class CCount{
+    private static int cnt = 0;
+    CCount(){
+        cnt++;
+    }
+    static int count(){
+        return cnt;
+    }
+    void setContZero(){
+        cnt = 0;
+    }
+    void setValue(int value){
+        cnt = value;
+    }
+}
