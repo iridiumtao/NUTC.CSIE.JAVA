4cb0f462e5573923c1b8f359735f5fb22c397126
歐東 <43561001+jerry960331@users.noreply.github.com>
2020-02-26 17:37:54 +0800

    200226 course

--- src/JavaClass/Ex191127ClassConception3/Ex4.java
+++ src/JavaClass/Ex191127ClassConception3/Ex4.java
@@ -1,32 +1,28 @@
 package JavaClass.Ex191127ClassConception3;
 
 public class Ex4 {
-    public static void main(String args[]){
-        Cbbb bb=new Cbbb();
-        bb.set_num(5,10);
+    public static void main(String args[]) {
+        Cbbb bb = new Cbbb();
+        bb.set_num(5, 10);
         bb.show();
     }
 }
 
-class Cbbb extends Caaa{
-    void set_num(int a, int b){
-        setNum1(a);
-        setNum2(b);
-    }
+class Cbbb extends Caaa {
+
 }
 
 class Caaa {
     private int num1;
     private int num2;
 
-    void show(){
-        System.out.printf("num1 = %d, num2 = %d",num1,num2);
+    void show() {
+        System.out.printf("num1 = %d, num2 = %d", num1, num2);
     }
-    void setNum1(int num){
-        num1 = num;
-    }
-    void setNum2(int num){
-        num2 = num;
+
+    void set_num(int a, int b) {
+        num1 = a;
+        num2 = b;
     }
 }
 
--- src/test2.java
+++ src/test2.java
@@ -8,36 +8,7 @@ public class test2 {
     public static void main(String[] args) {
         //this is a test for Lambda expression and Method References
 
-        int[] arr = {0,1,2,3,5,6,7,8,9,10};
-        for (int i : arr) {
-            System.out.println(i);
-        }
-
-
-
-
-        List<String> message = Arrays.asList("hi", "i", "want", "this", "all", "capitalized");
-        message.forEach(word -> System.out.println(StringUtils.capitalize(word)));
-
-        System.out.println();
-
-
-        for (int i = 0; i < 35; i++) {
-            if (i % 10 == 1){
-                switch (i){
-                    case 11:
-                        System.out.println("11");
-                        break;
-                    case 21:
-                        System.out.println("21");
-                        break;
-                    case 31:
-                        System.out.println("31");
-                        break;
-                    default:
-                        System.out.println();
-                }
-            }
-        }
+        String str = "G1O2G1L1E1";
+        System.out.println(Integer.parseInt(str));
     }
 }
